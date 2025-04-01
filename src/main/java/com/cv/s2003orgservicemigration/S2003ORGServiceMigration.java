94e6060ebf1cf256febafa2a23c771dfea237546
ram <ram_r@live.com>
2025-04-01 19:22:57 +0400

    BASE

---
+++ src/main/java/com/cv/s2003orgservicemigration/S2003ORGServiceMigration.java
@@ -0,0 +1,13 @@
+package com.cv.s2003orgservicemigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2003ORGServiceMigration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S2003ORGServiceMigration.class, args);
+    }
+
+}
